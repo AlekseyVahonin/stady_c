71e60e9a159e21b9842312e04a59bfee3defb8c9
Aleksey.V <aleksey.vahoninnd@gmail.com>
2023-07-22 18:27:18 +0300

    поиск n элемента массива

---
+++ example009_intro_arr_index/Program.cs
@@ -0,0 +1,24 @@
+﻿// Имеется массив из n элементов, требуется найти элемент массива равный find.
+
+int[] array = { 1, 2, 5, 5, 3 };
+int find = 5;
+
+int returnFindArray(int[] arr, int number)
+{
+    int result = -1;
+    int index = 0;
+    int count = arr.Length;
+
+    while (index < count)
+    {
+        if(arr[index] == number)
+        {
+            result = index;
+            break;
+        }
+        index++;
+    }
+    return result;
+}
+int result = returnFindArray(array, find);
+Console.WriteLine(result);
