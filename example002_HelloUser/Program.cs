3882d24d0b85f10d61308c83e064174e83d24101
Aleksey <aleksey.vahoninnd@gmail.com>
2023-07-29 17:01:14 +0300

    Replacement text

--- example002_HelloUser/Program.cs
+++ example002_HelloUser/Program.cs
@@ -1,10 +1,10 @@
-﻿Console.Write("Введите ваше имя: ");
-string userName = Console.ReadLine();
-if (userName.ToLower() == "маша")
-{
-    Console.WriteLine("Ого привет, " + userName);
-}
-else
-{
-    Console.WriteLine("Привет, " + userName);
-}
+﻿// Console.Write("Введите ваше имя: ");
+// // string userName = Console.ReadLine();
+// // if (userName.ToLower() == "маша")
+// {
+//     Console.WriteLine("Ого привет, " + userName);
+// }
+// else
+// {
+//     Console.WriteLine("Привет, " + userName);
+// }
---
+++ example010_Replacement_text/Program.cs
@@ -0,0 +1,26 @@
+﻿//Есть текст.
+//В тексте необходимо заменить все пробелы _,
+//буквы "к" заменить на "К",
+// буквы "с" замень на "С". 
+
+string textUser = "Мы вынуждены отталкиваться от того, что существующая"
+                + "теория однозначно фиксирует необходимость"
+                + "системы массового участия.";
+Console.WriteLine(textUser);
+
+string newText = ReplaceText(ReplaceText(ReplaceText(textUser, ' ', '_'), 'к', 'К'), 'с', 'С');
+Console.WriteLine(newText);
+
+string ReplaceText(string text, char symbol, char newSymbol)
+{
+    string result = String.Empty;
+    int length = text.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == symbol) result = result + newSymbol;
+        else result = result + text[i];
+    }
+    return result;
+}
+
