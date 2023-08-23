008f3f57371c1e2084325fd31bb3983534f1cdc6
Aleksey <aleksey.vahoninnd@gmail.com>
2023-08-23 12:58:18 +0300

    example013_Factorial

---
+++ example013_Factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿// Факториал рекурсивно
+
+int Factorial(int number)
+{
+    if (number == 1) return 1;
+    else return number * Factorial(number - 1);
+}
+
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine(Factorial(i));
+}
