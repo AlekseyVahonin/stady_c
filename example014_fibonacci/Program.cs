913934009757eeebd6603061b33fed6e8b665f14
Aleksey <aleksey.vahoninnd@gmail.com>
2023-08-23 13:51:39 +0300

    example014_fibonacci

---
+++ example014_fibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿// Фибоначи рекурсивно
+
+int Fibonacci(int number)
+{
+    if (number == 1 || number == 2) return 1;
+    else return Fibonacci(number - 1) + Fibonacci(number - 2);
+}
+
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
